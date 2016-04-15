1e30c72de13b22ad857cb42a0fcc28b143a6bed6
Yanis Urbis <yanis.urbis@gmail.com>
2016-04-15 16:48:10 +0300

    Revert "Revert "Publish and subscribe to DB, Private/Public button.""
    This reverts commit 97a6029eb7117a7e09df2cd5d70208700e16c4dc.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -41,9 +41,19 @@ class App extends Component {
       filteredTasks = filteredTasks.filter(task => !task.checked)
     }
 
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ))
+    return filteredTasks.map((task) => {
+
+      const currentUserId = this.props.currentUser && this.props.currentUser._id
+      const showPrivateButton = task.owner === currentUserId
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      )
+    })
   }
 
   render() {
@@ -91,6 +101,8 @@ App.propTypes = {
 }
 
 export default createContainer(() => {
+  Meteor.subscribe('tasks')
+
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
