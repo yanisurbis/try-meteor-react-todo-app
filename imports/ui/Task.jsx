478855065dfa00ca07b8f99bb776a787fc128032
Yanis Urbis <yanis.urbis@gmail.com>
2016-04-15 15:09:13 +0300

    Security with methods.
    Doesn't allow all users to write into DB.

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -2,16 +2,16 @@ import React, { Component, PropTypes } from 'react'
 
 import { Tasks } from '../api/tasks.js'
 
+import { Meteor } from 'meteor/meteor'
+
 export default class Task extends Component {
 
   toggleChecked() {
-    Tasks.update(this.props.task._id, {
-      $set: { checked: !this.props.task.checked },
-    })
+    Mateor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked)
   }
 
   deleteThisTask() {
-    Tasks.remove(this.props.task._id)
+    Meteor.call('tasks.remove', this.props.task._id)
   }
 
   render() {
@@ -35,7 +35,7 @@ export default class Task extends Component {
           <strong>{this.props.task.username}</strong> :
           {this.props.task.text}
         </span>
-        
+
       </li>
     )
   }
